223a4ac5f840d14b24ea5c998392da5547bc1ad9
Hyeongjun Jo <hjjo@superb-ai.com>
2021-12-10 13:16:08 +0900

    init

---
+++ packages/container/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MarketingApp from "./component/MarketingApp";
+
+export default () => {
+  return (
+    <div>
+      <MarketingApp />
+      <hr />
+      <h1>Hi there!</h1>
+    </div>
+  );
+};
